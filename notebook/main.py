1d715769a8afbf1c95fd31b16ebfe84563568ea6
AfiaFaith <faithafia@gmail.com>
2023-09-29 19:52:22 +0000

    Ticket 2: Gestion des erreurs

--- notebook/main.py
+++ notebook/main.py
@@ -3,26 +3,34 @@
 import os
 import pandas as pd
 
-# Récupération du jeu de données depuis un chemin local
-data_path = "../data/raw/raw_data.csv"
-data = pd.read_csv(data_path)
+try:
+    # Récupération du jeu de données depuis un chemin local
+    data_path = "../data/raw/raw_data.csv"
+    data = pd.read_csv(data_path)
 
-# Nettoyer les données : suppression des lignes avec des valeurs manquantes
-data = data.dropna()
+    # Nettoyer les données : suppression des lignes avec des valeurs manquantes
+    data = data.dropna()
 
-# Notre analyse ne concernera que le nombre de films réalisés par un auteur dans une ville donnée, donc nous allons sélectionner que 4 colonnes de notre donnée
-data = data[['titre', 'realisateur', 'nombre_de_film', 'Ville']]
+    # Notre analyse ne concernera que le nombre de films réalisés par un auteur dans une ville donnée, donc nous allons sélectionner que 4 colonnes de notre donnée
+    data = data[['titre', 'realisateur', 'nombre_de_film', 'Ville']]
 
-# Faire du feature engineering : nous allons créer une nouvelle colonne "titre_realisateur"
-data['titre_realisateur'] = data['titre'] + '_' + data['realisateur']  # Utilisation de '+' pour concaténer les valeurs
+    # Faire du feature engineering : nous allons créer une nouvelle colonne "titre_realisateur"
+    data['titre_realisateur'] = data['titre'] + '_' + data['realisateur']  # Utilisation de '+' pour concaténer les valeurs
 
-# Statistiques sommaires de notre donnée
-summary_stats = data.describe()
-print(summary_stats)
+    # Statistiques sommaires de notre donnée
+    summary_stats = data.describe()
+    print(summary_stats)
 
-# Création du répertoire "cleaned" s'il n'existe pas
-output_directory = 'data/cleaned'
-os.makedirs(output_directory, exist_ok=True)
+    # Création du répertoire "cleaned" s'il n'existe pas
+    output_directory = 'data/cleaned'
+    os.makedirs(output_directory, exist_ok=True)
 
-# Sauvegarde des données nettoyées dans le dossier "data/cleaned"
-data.to_csv(os.path.join(output_directory, 'cleaned.csv'), index=False)
+    # Sauvegarde des données nettoyées dans le dossier "data/cleaned"
+    data.to_csv(os.path.join(output_directory, 'cleaned.csv'), index=False)
+
+except FileNotFoundError as e:
+    print(f"Erreur: Le fichier de données '{data_path}' n'a pas été trouvé. Veuillez vérifier le chemin d'accès.")
+    # Gérer l'erreur ici ou ajouter une sortie propre
+except Exception as e:
+    print(f"Une erreur inattendue s'est produite: {str(e)}")
+    # Gérer l'erreur ici ou ajouter une sortie propre
