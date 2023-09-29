dd75fb5dd66e36ed42dd9887cf08b03f378a368b
AfiaFaith <faithafia@gmail.com>
2023-09-29 19:54:27 +0000

    Ticket 3: Utilisation de constantes

--- notebook/main.py
+++ notebook/main.py
@@ -3,16 +3,24 @@
 import os
 import pandas as pd
 
+# Constantes pour les chemins d'accès aux fichiers
+DATA_PATH = "../data/raw/raw_data.csv"
+OUTPUT_DIRECTORY = 'data/cleaned'
+
 try:
     # Récupération du jeu de données depuis un chemin local
-    data_path = "../data/raw/raw_data.csv"
-    data = pd.read_csv(data_path)
+    data = pd.read_csv(DATA_PATH)
+
+    # Validation des données d'entrée : vérifier la structure du fichier CSV
+    expected_columns = ['titre', 'realisateur', 'nombre_de_film', 'Ville']
+    if not all(col in data.columns for col in expected_columns):
+        raise ValueError("Le fichier de données n'a pas la structure attendue.")
 
     # Nettoyer les données : suppression des lignes avec des valeurs manquantes
     data = data.dropna()
 
     # Notre analyse ne concernera que le nombre de films réalisés par un auteur dans une ville donnée, donc nous allons sélectionner que 4 colonnes de notre donnée
-    data = data[['titre', 'realisateur', 'nombre_de_film', 'Ville']]
+    data = data[expected_columns]
 
     # Faire du feature engineering : nous allons créer une nouvelle colonne "titre_realisateur"
     data['titre_realisateur'] = data['titre'] + '_' + data['realisateur']  # Utilisation de '+' pour concaténer les valeurs
@@ -22,14 +30,16 @@ try:
     print(summary_stats)
 
     # Création du répertoire "cleaned" s'il n'existe pas
-    output_directory = 'data/cleaned'
-    os.makedirs(output_directory, exist_ok=True)
+    os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
 
     # Sauvegarde des données nettoyées dans le dossier "data/cleaned"
-    data.to_csv(os.path.join(output_directory, 'cleaned.csv'), index=False)
+    data.to_csv(os.path.join(OUTPUT_DIRECTORY, 'cleaned.csv'), index=False)
 
 except FileNotFoundError as e:
-    print(f"Erreur: Le fichier de données '{data_path}' n'a pas été trouvé. Veuillez vérifier le chemin d'accès.")
+    print(f"Erreur: Le fichier de données '{DATA_PATH}' n'a pas été trouvé. Veuillez vérifier le chemin d'accès.")
+    # Gérer l'erreur ici ou ajouter une sortie propre
+except ValueError as e:
+    print(f"Erreur: {str(e)}")
     # Gérer l'erreur ici ou ajouter une sortie propre
 except Exception as e:
     print(f"Une erreur inattendue s'est produite: {str(e)}")
